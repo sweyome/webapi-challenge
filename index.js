198aa52f1a12acfea21a595a573faa14e8d5e17a
sweyome <sweyome1@gmail.com>
2019-08-18 19:09:07 -0400

    all library intsalled

--- index.js
+++ index.js
@@ -12,3 +12,18 @@ I need this code, just don't know where, perhaps should make some middleware, do
 
 Go code!
 */
+
+const express = require('express');
+const port = 9000;
+const server = express();
+
+const projectModel = require('./projects/projectRoutes.js');
+const actionModel = require("./actions/actionRoutes.js");
+
+server.use(express.json());
+server.use("/projects", projectModel);
+server.use("/actions", actionModel);
+
+server.listen( port, () => {
+    console.log(`====== Server listening on port ${port} ======`);
+});
